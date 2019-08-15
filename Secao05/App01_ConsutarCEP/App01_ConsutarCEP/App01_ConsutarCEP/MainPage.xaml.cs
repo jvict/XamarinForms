5aa557ad280172a8f3359118ec34f611e011e7ae
João Victor Tiburcio <jvictor.jvt@gmail.com>
2019-08-15 12:57:01 -0300

    Aplicativo CEP 01 - Concluido

---
+++ Secao05/App01_ConsutarCEP/App01_ConsutarCEP/App01_ConsutarCEP/MainPage.xaml.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using App01_ConsutarCEP.Servico.Modelo;
+
+namespace App01_ConsutarCEP
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+        Editor obj = new Editor();
+   
+
+        string _filename = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "notes.txt");
+        public MainPage()
+        {
+            InitializeComponent();
+
+            BOTAO.Clicked += BuscarCEP;
+
+            if (File.Exists(_filename))
+            {
+               
+               obj.Text = File.ReadAllText(_filename);
+            }
+        }
+
+        private void BuscarCEP (object sender, EventArgs e)
+        {
+            //Logica do Programa
+
+            //Validações
+            string cep = CEP.Text.Trim(); // Remove todo espaço do inicio e do fim caso seja espaço em branco
+            if (isValidCEP(cep))
+            {
+                try
+                {
+                    Endereco end = ViaCEPServico.BuscarEnderecoURL(cep);
+
+                    if (end != null)
+                    {
+
+                        RESULTADO.Text = String.Format("Endereço: {2} - {3} - {0},{1} ", end.localidade, end.uf, end.logradouro, end.bairro);
+                    }
+                    else
+                    {
+                        DisplayAlert("ERRO", "O endereço não foi encontrado para o CEP informado: " + cep,"OK");
+                    }
+                }catch(Exception ex)
+                {
+                    DisplayAlert("Erro Critico", ex.Message, "Ok");
+                }
+            }
+            
+        }
+
+        private bool isValidCEP(string cep)
+        {
+            bool valido = true;
+
+            if(cep.Length != 8)
+            {
+                DisplayAlert("ERRO","CEP inválido ! O CEP deve conter 8 carateres.","OK");
+
+                valido = false;
+            }
+            int NovoCEP = 0;
+            if(!int.TryParse(cep, out NovoCEP))//Tente converter algo
+            {
+                
+                DisplayAlert("ERRO", "CEP inválido ! O CEP deve ser composto apenas por numeros.", "OK");
+                valido = false;
+            }
+            return valido;
+        }
+
+        public void OnSaveButtonClicked(object sender, EventArgs e)
+        {
+            File.WriteAllText(_filename, obj.Text);
+        }
+
+        public void OnDeleteButtonClicked(object sender, EventArgs e)
+        {
+            if (File.Exists(_filename))
+            {
+                File.Delete(_filename);
+            }
+            obj.Text = string.Empty;
+        }
+        public void Button_Clicked(object sender, System.EventArgs e)
+        {
+            count++;
+            ((Button)sender).Text = $"Você Clicou {count} vezes.";
+        }
+    }
+}
---
+++ Secao05/App01_ConsutarCEP/App01_ConsutarCEP/App01_ConsutarCEP/Servico/Modelo/ViaCEPServico.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using Newtonsoft.Json;
+using App01_ConsutarCEP.Servico.Modelo;
+
+
+namespace App01_ConsutarCEP.Servico.Modelo
+{
+    public class ViaCEPServico
+    {
+        //Metodo com URL que pesquisa CEP no site Via CEP
+        public static string EnderecoURL = "http://viacep.com.br/ws/{0}/json/";
+
+        public static Endereco BuscarEnderecoURL (string cep)
+        {
+            //Passando para a URL que pesquisa o endereço o CEP que o usuario deseja pesquisar
+            string NovoEndercoURL = string.Format(EnderecoURL, cep);
+
+            //Realizando a pesquisa na internet
+            WebClient wc = new WebClient();
+            string conteudo = wc.DownloadString(NovoEndercoURL);
+
+            //Convertendo o resultado da internet em umm obj da Classe Endereço
+            Endereco end = JsonConvert.DeserializeObject<Endereco>(conteudo);
+
+            if (end.cep == null) return null;
+
+            return end;            
+        }
+    }
+}
